7338c978d52399858e249136142d07a081627544
Weslley Gomes <wegomes@santillana.com>
2023-12-21 14:28:03 -0300

    Módulo 1 - Aula 4: Usando o GitHub Copilot Labs

--- PONG-COPILOT/sketch.js
+++ PONG-COPILOT/sketch.js
@@ -1,36 +1,57 @@
 // código base do p5.js para o projeto Pong Copilot
 // Autor: Weslley Gomes
 
-// posição da bola
-var x = 200;
-var y = 200;
+class Bola {
+    constructor() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.radius = 25;
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
 
-// velocidades da bola aleatórias usando função math.random
-var vx = Math.random() * 10 - 5;
-var vy = Math.random() * 10 - 5;
+    reset() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        // se tocar na borda horizontal, reseta no meio da tela
+        if (this.x < this.radius || this.x > width - this.radius) {
+            this.reset();
+        }
+
+        // se tocar na borda vertical, inverte a velocidade
+        if (this.y < this.radius || this.y > height - this.radius) {
+            this.vy *= -1;
+        }
+    }
+
+    draw() {
+        fill(255);
+        ellipse(this.x, this.y, 2 * this.radius, 2 * this.radius);
+    }
+}
+
+// cria uma nova bola
+let bola;
 
 // função setup do p5.js
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
+    background(0);
+    bola = new Bola();
 }
 
 // função draw do p5.js
 function draw() {
     // desenha um círculo branco
     background(0);
-    fill(255);
-    ellipse(x, y, 50, 50);
-    x += vx;
-    y += vy;
-
-    // se tocar na borda horizontal, inverte a velocidade
-    if (y < 25 || y > 375) {
-        vy *= -1;
-    }
-
-    // se tocar na borda vertical, inverte a velocidade
-    if (x < 25 || x > 375) {
-        vx *= -1;
-    }
+    bola.update();
+    bola.draw();
 }
-
